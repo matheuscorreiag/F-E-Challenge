664dd3465f84b3c818e966a31251b45532197669
Matheus Correia <matheuscorreiags@gmail.com>
2020-09-16 14:05:06 -0300

    v2

--- src/components/SearchBox/index.tsx
+++ src/components/SearchBox/index.tsx
@@ -1,19 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 
-import SearchIcon from "@material-ui/icons/Search";
+import LocationSearchingIcon from "@material-ui/icons/LocationSearching";
 import { Map, TileLayer, Marker } from "react-leaflet";
+import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button";
+import data from "../../data/neighborhoods.json";
 import "./styles.scss";
 import "../../common.scss";
 
 const SearchBox: React.FC = () => {
+  const [activeNeighbors, setActiveNeighbors] = useState(null);
   const initialPosition = [-7.163, -34.879];
+  const [modalShow, setModalShow] = React.useState(false);
+
+  function MyVerticallyCenteredModal(props) {
+    return (
+      <Modal
+        {...props}
+        size="lg"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+        <Modal.Header closeButton>
+          <Modal.Title id="contained-modal-title-vcenter">
+            Modal heading
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <h4>Centered Modal</h4>
+          <p>
+            Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
+            dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta
+            ac consectetur ac, vestibulum at eros.
+          </p>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button onClick={props.onHide}>Close</Button>
+        </Modal.Footer>
+      </Modal>
+    );
+  }
   return (
     <>
       <div className="search-container">
         <div className="input-button">
           <input type="text" placeholder="  Digite aqui" />
           <button>
-            <SearchIcon />
+            <LocationSearchingIcon />
           </button>
         </div>
         <div className="map">
@@ -21,6 +54,15 @@ const SearchBox: React.FC = () => {
             <TileLayer
               attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
               url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Marker
+              key="fodase"
+              position={[-7.163, -34.879]}
+              onClick={() => setModalShow(true)}
+            />
+            <MyVerticallyCenteredModal
+              show={modalShow}
+              onHide={() => setModalShow(false)}
             />
           </Map>
         </div>
